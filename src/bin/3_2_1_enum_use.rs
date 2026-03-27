606b3ba61c1354830d30a6bc933e79da0720f94a
qiekn <qiekndev@gmail.com>
2026-03-27 13:39:10 +0800

    ep3: custom types

---
+++ src/bin/3_2_1_enum_use.rs
@@ -0,0 +1,33 @@
+#![allow(dead_code)]
+
+enum Status {
+    Rich,
+    Poor,
+}
+
+enum Work {
+    Civilian,
+    Soldier,
+}
+
+fn main() {
+    use Status::{Poor, Rich};
+    use Work::*;
+
+    // `Poor` 等价于 `Status::Poor`。
+    let status = Poor;
+    // `Civilian` 等价于 `Work::Civilian`。
+    let work = Civilian;
+
+    match status {
+        // 注意这里没有用完整路径，因为上面显式地使用了 `use`。
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        // 再次注意到没有用完整路径。
+        Civilian => println!("Civilians work!"),
+        Soldier => println!("Soldiers fight!"),
+    }
+}
---
+++ src/bin/3_3_0_constants.rs
@@ -0,0 +1,19 @@
+static LANGUAGE: &'static str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    // 在一般函数中访问常量
+    n > THRESHOLD
+}
+
+fn main() {
+    let n = 16;
+
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // 报错！不能修改一个 `const` 常量。
+    // THRESHOLD = 5;
+    // 改正 ^ 注释掉此行
+}
