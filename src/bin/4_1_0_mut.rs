c58d5058ede32694f6f3f4a5628ebcf7c3a99cc7
qiekn <qiekndev@gmail.com>
2026-03-29 00:06:45 +0800

    ep4: variable bindings

---
+++ src/bin/4_1_0_mut.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let a_binding;
+
+    {
+        let x = 2;
+
+        a_binding = x * x; // 4
+    }
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // 只做了声明，没有做初始化
+    // println!("another binding: {}", another_binding);
+
+    another_binding = 1;
+    println!("another binding: {}", another_binding);
+}
