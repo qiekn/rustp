d9a89506cc7526bcf8c83a3c61a14fbaf535e91e
qiekn <qiekndev@gmail.com>
2026-04-09 11:21:25 +0800

    ep10: modules

---
+++ src/bin/10_5_0_file-hierarchy/my.rs
@@ -0,0 +1,16 @@
+mod inaccessible;
+pub mod nested;
+
+pub fn function() {
+  println!("called `my::function()");
+}
+
+fn private_function() {
+  println!("called `my::private_function()");
+}
+
+pub fn indirect_access() {
+  println!("called `my::indirect_accesss()");
+
+  private_function();
+}
